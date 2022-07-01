68439f20d78b639e7a8a04f0177d17a63472640c
Tekdey <devw.nbardi@gmail.com>
2022-07-01 19:53:05 +0200

    ✨ - initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+
+const App = () => {
+  return (
+    <Stack>
+      <Alert severity="success">Hello, world</Alert>
+    </Stack>
+  );
+};
+
+export default App;
